d80ab6f8c4358166e520f181f112a8c82685f8fe
pemapol <t.pemapol@gmail.com>
2022-07-27 09:24:14 +0700

    add router getallrooms

--- router/router.go
+++ router/router.go
@@ -1,9 +1,14 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/autousers/backend_reserve/middleware"
+	"github.com/gorilla/mux"
+)
 
 func Router() *mux.Router {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/api/room", middleware.GetAllRoom).Methods("GET", "OPTIONS")
 
 	return router
 }
